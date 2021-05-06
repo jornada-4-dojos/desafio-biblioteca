8fdaec4b4aeab833a67138c62666774d82657385
Diego Sousa <diegors90@gmail.com>
2021-05-05 21:28:50 -0300

    [wip] novo emprestimo

--- src/main/java/com/deveficiente/biblioteca/novoemprestimo/EmprestimoRequest.java
+++ src/main/java/com/deveficiente/biblioteca/novoemprestimo/EmprestimoRequest.java
@@ -1,6 +1,19 @@
 package com.deveficiente.biblioteca.novoemprestimo;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+import com.deveficiente.biblioteca.cadastroexemplar.TipoCirculacao;
+
 public class EmprestimoRequest {
 
+	@NotNull
 	private Long idUsuario;
+	@NotNull
+	private Long idLivro;
+	@Positive
+	@Max(value = 60)
+	private int dias;
+	private TipoCirculacao tipoCirculacao;
 }
--- src/main/java/com/deveficiente/biblioteca/novoemprestimo/NovoEmprestimoController.java
+++ src/main/java/com/deveficiente/biblioteca/novoemprestimo/NovoEmprestimoController.java
@@ -3,13 +3,15 @@ package com.deveficiente.biblioteca.novoemprestimo;
 import javax.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NovoEmprestimoController {
 
 	@PostMapping("/emprestimo")
-	public EmprestimoResponse criarEmprestimo(@Valid EmprestimoRequest request) {
-		
+	public Long criarEmprestimo(@RequestBody @Valid EmprestimoRequest request) {
+		var emprestimo = request.toModel();
+		return 0L;
 	}
 }
