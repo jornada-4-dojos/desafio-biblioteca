9076abdaac31105d90cec631bd208a6517413d37
Marcelo Pereira <marcelopereira@MBP-de-Marcelo.lan>
2021-05-14 00:30:50 +0100

    [WIP] - validar prazo empréstimo

--- src/main/java/com/deveficiente/biblioteca/novoemprestimo/ChecaPrazoValidator.java
+++ src/main/java/com/deveficiente/biblioteca/novoemprestimo/ChecaPrazoValidator.java
@@ -1,38 +1,35 @@
 package com.deveficiente.biblioteca.novoemprestimo;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import com.deveficiente.biblioteca.novousuario.Usuario;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
-import com.deveficiente.biblioteca.novousuario.TipoUsuario;
-import com.deveficiente.biblioteca.novousuario.Usuario;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Component
-public class ChecaPrazoValidator implements Validator{
-	@PersistenceContext
-	private EntityManager entityManager;
-	@Override
-	public boolean supports(Class<?> clazz) {
-		return EmprestimoRequest.class.isAssignableFrom(clazz);
-	}
+public class ChecaPrazoValidator implements Validator {
+    @PersistenceContext
+    private EntityManager entityManager;
 
-	@Override
-	public void validate(Object target, Errors errors) {
-		if(errors.hasErrors()) 
-			return;
-		
-		EmprestimoRequest request = (EmprestimoRequest)target;
-		Usuario usuario = entityManager.find(Usuario.class, request.getIdUsuario());
-		
-		if(TipoUsuario.PADRAO.equals(usuario.getTipoUsuario())) {
-			if(request.getDias() == null) {
-				errors.rejectValue("dias", "campo 'dias' precisa estar preenchido para usuário padrão");
-			}
-		}
-		
-	}
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return EmprestimoRequest.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        if (errors.hasErrors())
+            return;
+
+        EmprestimoRequest request = (EmprestimoRequest) target;
+        Usuario usuario = entityManager.find(Usuario.class, request.getIdUsuario());
+
+        if (!usuario.getTipoUsuario().prazoValido(request.getDias())) {
+            errors.rejectValue("dias", "campo 'dias' precisa estar preenchido para usuário padrão");
+        }
+
+    }
 
 }
--- src/main/java/com/deveficiente/biblioteca/novousuario/TipoUsuario.java
+++ src/main/java/com/deveficiente/biblioteca/novousuario/TipoUsuario.java
@@ -1,5 +1,17 @@
 package com.deveficiente.biblioteca.novousuario;
 
 public enum TipoUsuario {
-    PADRAO, PEQUISADOR;
+    PADRAO {
+        @Override
+        public boolean prazoValido(Integer dias) {
+            return dias != null && dias <= 60;
+        }
+    }, PEQUISADOR {
+        @Override
+        public boolean prazoValido(Integer dias) {
+            return dias <= 60;
+        }
+    };
+
+    public abstract boolean prazoValido(Integer dias);
 }
