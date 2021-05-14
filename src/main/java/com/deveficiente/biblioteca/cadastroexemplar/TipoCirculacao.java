ccaaf25c8678df6940844d29e3b74046410e8214
Marcelo Pereira <marcelopereira@MBP-de-Marcelo.lan>
2021-05-14 01:53:51 +0100

    Finaliza validação do tipo circulação pelo usuário

--- src/main/java/com/deveficiente/biblioteca/cadastroexemplar/TipoCirculacao.java
+++ src/main/java/com/deveficiente/biblioteca/cadastroexemplar/TipoCirculacao.java
@@ -1,5 +1,21 @@
 package com.deveficiente.biblioteca.cadastroexemplar;
 
+import com.deveficiente.biblioteca.novousuario.TipoUsuario;
+import org.springframework.util.Assert;
+
 public enum TipoCirculacao {
-    LIVRE, RESTRITO;
+    LIVRE {
+        @Override
+        public boolean aceitaEmprestimo(TipoUsuario tipoUsuario) {
+            return true;
+        }
+    }, RESTRITO {
+        @Override
+        public boolean aceitaEmprestimo(TipoUsuario tipoUsuario) {
+            Assert.notNull(tipoUsuario, "O tipo usuário não pode ser nulo");
+            return TipoUsuario.PESQUISADOR.equals(tipoUsuario);
+        }
+    };
+
+    public abstract boolean aceitaEmprestimo(TipoUsuario tipoUsuario);
 }
--- src/main/java/com/deveficiente/biblioteca/novoemprestimo/TipoCirculacaoUsuarioValidator.java
+++ src/main/java/com/deveficiente/biblioteca/novoemprestimo/TipoCirculacaoUsuarioValidator.java
@@ -1,25 +1,39 @@
 package com.deveficiente.biblioteca.novoemprestimo;
 
+import com.deveficiente.biblioteca.novousuario.Usuario;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 @Component
-public class TipoCirculacaoUsuarioValidator implements Validator{
+public class TipoCirculacaoUsuarioValidator implements Validator {
 
-	@Override
-	public boolean supports(Class<?> clazz) {
-		return EmprestimoRequest.class.isAssignableFrom(clazz);
-	}
+	@PersistenceContext
+	private EntityManager manager;
 
-	@Override
-	public void validate(Object target, Errors errors) {
-		if(errors.hasErrors())
-			return;
-		
-		EmprestimoRequest request = (EmprestimoRequest)	target;
-		
-		
-	}
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return EmprestimoRequest.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        if (errors.hasErrors())
+            return;
+
+        EmprestimoRequest request = (EmprestimoRequest) target;
+
+        Usuario usuario = this.manager.find(Usuario.class, request.getIdUsuario());
+		Assert.notNull(usuario, "O usuário não pode ser nulo");
+
+        if (!request.getTipoCirculacao().aceitaEmprestimo(usuario.getTipoUsuario())) {
+			errors.rejectValue("tipoCirculacao", null, "O usuário não pode pedir empréstimo para o tipo de circulação enviada");
+        }
+
+    }
 
 }
